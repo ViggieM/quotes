506b19fd94d49e23da6902f4e45549739be8b3d5
Victor <victor.movileanu@check24.de>
2024-02-05 16:12:47 +0100

    set up tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,14 +8,15 @@ const config = {
   preprocess: [
     mdsvex({
       extensions: ['.md', '.svx'],
-    })
+    }),
+    vitePreprocess()
   ],
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter()
-  }
+  },
 };
 
 export default config;
