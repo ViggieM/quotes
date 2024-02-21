5aa4fe6acb1cb4bf24c1ff18f50efdca02085ea6
Victor <victor.movileanu@check24.de>
2024-02-21 19:06:32 +0100

    add some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,28 @@
   export let data
 </script>
 
-<h1>Quotes</h1>
+<main>
+  <h1 class="text-xl">Quotes</h1>
 
-{#each data.quotes as quote}
-  <article>
-    <a href="{quote.url}">
-      <h2>{quote.title}</h2>
-    </a>
-    {#if quote.tags}
-      <ul>
-        {#each quote.tags as tag}
-          <li>{tag}</li>
-        {/each}
-      </ul>
-    {/if}
-  </article>
-{/each}
+  <div class="grid md:grid-cols-3 gap-2">
+    {#each data.quotes as quote}
+      <article class="border rounded p-2">
+
+        <div class="flex gap-2 mb-2">
+          <h2>{quote.title}</h2>
+          <a href="{quote.url}">
+            <span class="text-xs underline text-teal-400 italic">read</span>
+          </a>
+        </div>
+
+        {#if quote.tags}
+          <ul class="flex gap-1">
+            {#each quote.tags as tag}
+              <li class="border px-2 py-1 bg-yellow-200 text-xs rounded">{tag}</li>
+            {/each}
+          </ul>
+        {/if}
+      </article>
+    {/each}
+  </div>
+</main>
