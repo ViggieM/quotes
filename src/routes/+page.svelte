880a141a5be41dc281a9131441cce87a47ce88a8
Victor <victormovileanu@gmail.com>
2024-02-22 23:21:10 +0100

    make entire article clickable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,23 +7,22 @@
 
   <div class="grid md:grid-cols-3 gap-2">
     {#each data.quotes as quote}
-      <article class="border rounded p-2">
+      <a href="{quote.url}">
+        <article class="border rounded p-2">
 
-        <div class="flex gap-2 mb-2">
-          <h2>{quote.title}</h2>
-          <a href="{quote.url}">
-            <span class="text-xs underline text-teal-400 italic">read</span>
-          </a>
-        </div>
+          <h2 class="mb-2">{quote.title}</h2>
 
-        {#if quote.tags}
-          <ul class="flex gap-1">
-            {#each quote.tags as tag}
-              <li class="border px-2 py-1 bg-yellow-200 text-xs rounded">{tag}</li>
-            {/each}
-          </ul>
-        {/if}
-      </article>
+          {#if quote.tags}
+            <ul class="flex gap-1">
+              {#each quote.tags as tag}
+                  <li class="border bg-yellow-200 text-xs rounded">
+                    <a class="block px-2 py-1" href="#">{tag}</a>
+                  </li>
+              {/each}
+            </ul>
+          {/if}
+        </article>
+      </a>
     {/each}
   </div>
 </main>
