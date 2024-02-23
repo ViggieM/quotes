8f31350404daa880f729f1d7545f8f653b6e1538
Victor <victormovileanu@gmail.com>
2024-02-24 00:44:27 +0100

    generate static site

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# [Quotes App](https://quotes.wherehowhy.com/)
+# Quotes App
 
-A svelte app to display quotes from markdown files.
+[![Netlify Status](https://api.netlify.com/api/v1/badges/e8024c82-4f6c-441c-a484-01576825edcc/deploy-status)](https://app.netlify.com/sites/glittery-pony-a5b5c0/deploys)
+
+A svelte app to display quotes from markdown files. Visit [quotes.wherehowhy.com](https://quotes.wherehowhy.com/).
+
+## For development
 
 * `npm run dev` to start dev server
 * `npm run build` to build
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 import {mdsvex} from 'mdsvex'
 
@@ -13,16 +13,15 @@ const config = {
   ],
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    })
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
   },
 };
 
